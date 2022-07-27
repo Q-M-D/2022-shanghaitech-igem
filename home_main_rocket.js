258f01fdcaa77c8635d1353ef7757aaac90074c8
zhujl <zhujl@shanghaitech.edu.cn>
2022-07-27 15:47:44 +0800

    change flame animation

--- home_main_rocket.js
+++ home_main_rocket.js
@@ -2,10 +2,21 @@ var div_rocket = document.getElementsByClassName("home_main_rocket")[0];
 var rocket = div_rocket.getElementsByTagName("img");
 rocket[0].style.display = "block";
 rocket[4].style.display = "block";
+var time = 0;
+
+
+setInterval(function(){
+    time=time+1;
+    var tmp = time % 6+1;
+    document.getElementById("flame1").src="image/Flame/stage1/"+tmp+".png";
+    document.getElementById("flame2").src="image/Flame/stage2/"+tmp+".png";
+    tmp = tmp%4+1;
+    document.getElementById("flame3").src="image/Flame/stage3/"+tmp+".png";
+},100);
 
 window.onscroll = function () {
     var dis=document.documentElement.scrollTop || document.body.scrollTop;
-    console.log(dis);
+    // console.log(dis);
     if (dis < 200){
         rocket[0].style.display = "block";
         rocket[1].style.display = "none";
@@ -33,11 +44,9 @@ window.onscroll = function () {
         rocket[5].style.display = "none";
         rocket[6].style.display = "block";
         rocket[1].style.animation = "rocket_wobble 0.1s linear 0s infinite";
-        rocket[6].style.animation = "none";
-        rocket[2].style.animation = "rocket_up 3s ease";
-        rocket[3].style.animation = "rocket_low 3s ease";
-        rocket[2].style.animationFillMode = "forwards";
-        rocket[3].style.animationFillMode = "forwards";
+        rocket[6].style.animation = "fire_flame 1s linear 0s forwards";
+        rocket[2].style.animation = "rocket_up 3s ease forwards";
+        rocket[3].style.animation = "rocket_low 3s ease forwards";
     }
     else {
         rocket[0].style.display = "none";
@@ -47,9 +56,7 @@ window.onscroll = function () {
         rocket[4].style.display = "none";
         rocket[5].style.display = "none";
         rocket[6].style.display = "block";
-        rocket[1].style.animation = "rocket_to_land 3s ease";
-        rocket[6].style.animation = "rocket_to_land_flame 3s ease";
-        rocket[1].style.animationFillMode = "forwards";
-        rocket[6].style.animationFillMode = "forwards";
+        rocket[1].style.animation = "rocket_to_land 3s ease forwards";
+        rocket[6].style.animation = "rocket_to_land_flame 3s ease forwards";
     }
 }
